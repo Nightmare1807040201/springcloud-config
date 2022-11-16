75c80a1fc5cafc7b1075d36d23a7c3988a19ff72
bh <bh@bh.com>
2022-11-16 13:31:10 +0800

    first commit

---
+++ src/main/java/com/bh/Test.java
@@ -0,0 +1,16 @@
+package com.bh;
+
+/**
+ * @author:binhao
+ * @date:2022/11/3,22:34
+ * @project_name:git-test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+         System.out.println("pull test");
+    }
+}
